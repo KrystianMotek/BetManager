6fd2448502ae9af9135d486fe8e804c274b53eef
KrystianMotek <krystianmotek308@gmail.com>
2025-01-18 22:35:00 +0100

    define service interface

--- src/Domain/Services/ICouponService.cs
+++ src/Domain/Services/ICouponService.cs
@@ -1,10 +1,14 @@
 using BetManager.Domain.Models;
+using BetManager.Application.Models.DTO;
 
 namespace BetManager.Domain.Services
 {
     public interface ICouponService
     {
+        Task<List<CouponDTO>> GetCouponsAsync();
         Task<Coupon?> GetCouponByIdAsync(Guid id);
-        Task<Coupon?> GetCouponByCouponNumberAsync(string couponNumber); 
+        Task<Coupon> CreateCouponAsync(CreateCouponDTO createCouponDTO);
+        Task<Coupon> UpdateCouponAsync(CouponDTO couponDTO);
+        Task DeleteCouponAsync(Guid id);
     }
 }
